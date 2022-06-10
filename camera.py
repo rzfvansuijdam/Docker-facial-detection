a43e5720e1153a70bbada7962f3eb37f1cdc64e5
rzfvansuijdam <rzfvansuijdam@gmail.com>
2022-06-10 09:47:00 +0200

    pushed work

---
+++ camera.py
@@ -0,0 +1,28 @@
+import cv2
+
+cascPath = 'haarcascade_frontalface_dataset.xml'  # dataset
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture('rtsp://riley:okok@192.168.144.203:8080/_channel=0_stream=0.sdp')  # 0 for web camera live stream
+
+
+def camera_stream():
+     # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the resulting frame in browser
+    return cv2.imencode('.jpg', frame)[1].tobytes()
